6379f825d9284e2f1b97b554dbf4b5b27697a4e5
Kevin Bluer <kevin@bluer.com>
2021-12-09 11:42:06 -0600

    fixing the migration script

--- migrations/2_nft_contract.js
+++ migrations/2_nft_contract.js
@@ -1,18 +1,14 @@
 const NFTTicket = artifacts.require("NFTTicket");
 const SalesManager = artifacts.require("SalesManager");
 
-module.exports = function (deployer) {
-  let nftTicket;
-  deployer.deploy(NFTTicket).then((res) => {
-    console.log(res);
-  });
-
-  console.log("---------- Y %O", nftTicket);
+module.exports = async function (deployer) {
+  
+  await deployer.deploy(NFTTicket);
 
   const salesManager = deployer.deploy(
     SalesManager,
     10,
     (1e18).toString(),
-    nftTicket.address
+    NFTTicket.address
   );
 };
